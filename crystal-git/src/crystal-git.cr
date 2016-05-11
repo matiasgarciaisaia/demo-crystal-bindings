bebd2263f50beb045a20d0556568483fe3faed62
Matias Garcia Isaia <mgarcia@manas.com.ar>
2016-05-11 16:42:29 -0300

    Bindings

--- crystal-git/src/crystal-git.cr
+++ crystal-git/src/crystal-git.cr
@@ -1,5 +1,8 @@
 require "./crystal-git/*"
 
 module Crystal::Git
-  # TODO Put your code here
+  LibGit.git_libgit2_init()
+  LibGit.git_libgit2_version(out major, out minor, out rev)
+  puts "Hi there! Crystal here using libgit2 v#{major}.#{minor}.#{rev}"
+  LibGit.git_libgit2_shutdown()
 end
---
+++ crystal-git/src/crystal-git/libgit.cr
@@ -0,0 +1,7 @@
+@[Link("git2", ldflags: "-L#{__DIR__}/../../../libgit2/build/")]
+lib LibGit
+  fun git_libgit2_init()
+  fun git_libgit2_shutdown()
+  fun git_libgit2_version(major: LibC::Int*, minor: LibC::Int*, rev: LibC::Int*)
+  # C bindings are fun!
+end
