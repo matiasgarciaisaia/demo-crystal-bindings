ecdcf65bf6f8f7ba5a4d355a09ca18683bd28e97
Matias Garcia Isaia <mgarcia@manas.com.ar>
2016-05-11 16:46:50 -0300

    Git object

--- crystal-git/src/crystal-git.cr
+++ crystal-git/src/crystal-git.cr
@@ -1,8 +1,4 @@
 require "./crystal-git/*"
 
-module Crystal::Git
-  LibGit.git_libgit2_init()
-  LibGit.git_libgit2_version(out major, out minor, out rev)
-  puts "Hi there! Crystal here using libgit2 v#{major}.#{minor}.#{rev}"
-  LibGit.git_libgit2_shutdown()
-end
+major, minor, rev = Git.version
+puts "Hi there! Crystal here using libgit2 v#{major}.#{minor}.#{rev}"
---
+++ crystal-git/src/crystal-git/git.cr
@@ -0,0 +1,12 @@
+require "./*"
+
+module Git
+  LibGit.git_libgit2_init()
+
+  extend self
+
+  def version
+    LibGit.git_libgit2_version(out major, out minor, out rev)
+    [major, minor, rev]
+  end
+end
